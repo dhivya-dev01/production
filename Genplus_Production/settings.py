7a1954fda841e460e655830b704c6c950bb1e747
dhivya-dev01 <genplusdev1@gmail.com>
2025-10-17 11:33:27 +0530

    commit

--- Genplus_Production/settings.py
+++ Genplus_Production/settings.py
@@ -131,17 +131,30 @@ SIMPLE_JWT = {
 
 
 
-
-DATABASES = { 
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'genplus_production',
-        'USER': 'root',  
-        'PASSWORD': '',
-        'HOST': 'localhost', 
-        'PORT': '3306',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
-}  
+}
+
+# DATABASES = { 
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'genplus_production',
+#         'USER': 'root',  
+#         'PASSWORD': '',
+#         'HOST': 'localhost', 
+#         'PORT': '3306',
+#     }
+# }  
 
 
 
